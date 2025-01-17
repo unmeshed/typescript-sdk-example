74c3a01e0bda39cff521d19cd9fdefbd935da75b
boney9 <boneysekh@gmail.com>
2025-01-17 02:53:48 -0800

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
-import {greet, greetWithType, MyType} from '@unmeshed/unmeshed-js-sdk'
+import {greet, greetWithType, MyType, UnmeshedClient} from '@unmeshed/unmeshed-js-sdk'
+import {} from '@unmeshed/unmeshed-js-sdk'
 
-const val : MyType = {
+const val: MyType = {
     name: 'String'
 }
 console.log(greet("hello world"));
 console.log(greetWithType(val));
+
+const unmeshedClient = new UnmeshedClient({
+    baseUrl: 'http://localhost',
+    port: 8080,
+    authToken: 'iBPNno0dOpJ74bBy64l5',
+    clientId: '0d368ea2-7ed7-4fc6-8506-d5d5115fce55'
+});
+
+unmeshedClient.registerWorkers();
+unmeshedClient.startPolling();
+
+
+unmeshedClient.getProcessData();
